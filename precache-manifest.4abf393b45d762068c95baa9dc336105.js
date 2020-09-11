a100342a21a2473fee7a21857adbc4e89d01893e
Nekrasovp <7581719+nekrasovp@users.noreply.github.com>
2020-09-11 13:53:14 +0300

    Updates

---
+++ precache-manifest.4abf393b45d762068c95baa9dc336105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ff6d9b76c8aa5d005873ea37119c01",
+    "url": "/blog-react/index.html"
+  },
+  {
+    "revision": "3de18efa47c08d4c416a",
+    "url": "/blog-react/static/css/main.b251ebb4.chunk.css"
+  },
+  {
+    "revision": "adb71041dec007928821",
+    "url": "/blog-react/static/js/2.cef50078.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog-react/static/js/2.cef50078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de18efa47c08d4c416a",
+    "url": "/blog-react/static/js/main.011bd381.chunk.js"
+  },
+  {
+    "revision": "1036d163d515442c3f5a",
+    "url": "/blog-react/static/js/runtime-main.fe59f0cc.js"
+  }
+]);
